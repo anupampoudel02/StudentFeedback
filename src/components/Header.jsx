88e99ec982ec558998935f9943b88154b8c2eaad
rabinjungkarki <np03cs4s240128@heraldcollege.edu.np>
2025-05-08 23:32:54 +0545

    chore: dashboard link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Module/InputDesign.module.css";
 
 function Header({ title, buttonText }) {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>{title}</h1>
-      <button className={styles.dashboardButton}>{buttonText}</button>
+      <Link to={"/dashboard"} className={styles.dashboardButton}>{buttonText}</Link>
     </header>
   );
 }
